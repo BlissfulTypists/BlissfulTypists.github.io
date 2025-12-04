267fdb177005fe8de813a72b73deac8705c99525
Madelyn H <madelynghuang@gmail.com>
2025-12-04 17:38:40 -0500

    Comment out animated button

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import TextCarousel from "./_components/TextCarousel.js";
 export default function Page() {
     return (
         <>
-            <button className={styles.animatedButton}></button>
+            {/* <button className={styles.animatedButton}></button> */}
             <Image src="/main_logo.png" alt="[DRE4M] Blissful Typists - Main Logo" width={1280*0.5} height={1024*0.5} className="mainLogo" />
             <TextCarousel />
             <p><b>This does not currently apply, but in the future:</b> Unfortunately, Nitro Type has a limit of 50 team members per team. This means that we unfortunately may have to remove people from the team and/or prevent new people from joining. We are truly sorry about this. Playing Nitro Type often can increase your chances of staying on the team.</p>
